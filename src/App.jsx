c8074db58e83e807e0db18f5054a747a5750a197
jackyan63@gmail.com <jackyan63@gmail.com>
2018-01-22 01:51:05 -0500

    Add material ui card

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import he from 'he';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import axios from 'axios';
 
 class App extends React.Component {
    render() {
       return (
-         <div>
-            <h1>{this.props.title}</h1>
-            <div>{this.props.content}</div>
-         </div>
+      	<MuiThemeProvider>
+	         <div>
+	         	<Card>
+	         	    <CardHeader title= {this.props.title} 
+					      actAsExpander={true}
+					      showExpandableButton={true}/>
+	            <CardText>{this.props.content}</CardText>
+	            <CardActions id = "button"></CardActions>
+	            </Card>
+	         </div>
+         </MuiThemeProvider>
       );
    }
 }
+
+handleClick();
+
+function handleClick(e){
+	let rng = Math.floor(Math.random() * (10000000 - 1 + 1)) + 1;
+	let apiUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_=" + rng;
+	axios.get(apiUrl)
+	.then(function (response) {
+		//alert("clicked");
+		//alert(response.data[0].content);
+		ReactDOM.render(
+		<App title={response.data[0].title} content = {he.decode(response.data[0].content).replace(/(<([^>]+)>)/ig,"")}/>,
+		document.getElementById('app')
+		);
+	})
+	.catch(function (error) {
+		console.log(error);
+	});
+}
+
 export default App;
--- src/QuoteButton.jsx
+++ src/QuoteButton.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import RaisedButton from 'material-ui/RaisedButton';
 import axios from 'axios';
-import App from './app.jsx'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import he from 'he';
+import App from './app.jsx';
 
 class QuoteButton extends React.Component {
   // This syntax ensures `this` is bound within handleClick.
@@ -9,22 +12,22 @@ class QuoteButton extends React.Component {
 
   render() {
     return (
-      <button onClick={handleClick}>
-        Click me
-      </button>
+    <MuiThemeProvider>
+      <RaisedButton onClick={handleClick} label="New Quote" />
+      < /MuiThemeProvider>
     );
   }
 }
 
 function handleClick(e){
-	const rng = Math.floor(Math.random() * (10000000 - 1 + 1)) + 1;
-	const apiUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_=" + rng;
+	let rng = Math.floor(Math.random() * (10000000 - 1 + 1)) + 1;
+	let apiUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_=" + rng;
 	axios.get(apiUrl)
 	.then(function (response) {
 		//alert("clicked");
 		//alert(response.data[0].content);
 		ReactDOM.render(
-		<App title={response.data[0].title} content = {unescape(response.data[0].content).replace(/(<([^>]+)>)/ig,"")}/>,
+		<App title={response.data[0].title} content = {he.decode(response.data[0].content).replace(/(<([^>]+)>)/ig,"")}/>,
 		document.getElementById('app')
 		);
 	})
